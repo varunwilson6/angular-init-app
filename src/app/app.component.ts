0b6310ef145cea6ba1edc71429504e7b32fc2fb6
varunwilson <varunwilson6@gmail.com>
2021-05-16 17:43:09 +0530

    New components added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css']
+  styles: [`
+  h3 {
+    color: dodgerblue
+  }
+  p {
+    color: darkgrey
+  }
+  `]
 })
 export class AppComponent {
 }
